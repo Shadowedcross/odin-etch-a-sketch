8ff85aa337332aca4948c3b4ffdf4f86dc53e6b1
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-03 13:11:29 +0100

    Create initial files and display the grid
    Created a README.md to describe the project. Made, and edited, the files required to display a grid of divs on screen.

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+The purpose of this repository is to demonstrate my understanding of DOM manipulation, using what I've learned from 
+The Odin Project.
+
+[Here is the TOP project assignment page](https://www.theodinproject.com/lessons/foundations-etch-a-sketch)
+
+The project is to create an Etch-a-Sketch that is                       adjustable, using Flexbox and JavaScript.
---
+++ scripts/javascript.js
@@ -0,0 +1,24 @@
+let divContainer
+let rowCount= 16;
+let columnCount = 16;
+
+if (document.readyState === "loading" ) {
+    document.addEventListener("DOMContentLoaded", generateGrid)
+} else {
+    generateGrid();
+}
+
+function generateGrid () {
+    divContainer = document.querySelector(".div-container");
+    console.log("generating")
+    for (let y = 0; y < rowCount; y++) {
+        for (let x = 0; x < columnCount; x++) {
+            let newDiv = document.createElement("div");
+            newDiv.className = `div ${y}x${x}`
+            divContainer.appendChild(newDiv);
+        }
+    }
+    console.log(divContainer.childElementCount)
+}
+
+
